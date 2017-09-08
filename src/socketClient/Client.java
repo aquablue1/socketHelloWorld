140c704b29ac89769ca388fe224103066db71f35
ping <94apieceofcake@gmail.com>
2017-09-08 17:57:42 -0600

    comments added

--- src/socketClient/Client.java
+++ src/socketClient/Client.java
@@ -8,14 +8,15 @@ import javax.swing.JOptionPane;
 
 /**
  * Trivial client for the data server
+ * http://cs.lmu.edu/~ray/notes/javanetexamples/ 
  * */
 public class Client {
 	
 	public static void main(String[] args) throws IOException {
 		String serverAddress = JOptionPane.showInputDialog(
 				"Enter IP address: \n" + 
-				"running the data port on 9090:");
-		Socket s = new Socket(serverAddress, 9090);
+				"running the data port on 9091:");
+		Socket s = new Socket(serverAddress, 9091);
 		BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
 		String answer = input.readLine();
 		JOptionPane.showMessageDialog(null, answer);
--- src/socketServer/Server.java
+++ src/socketServer/Server.java
@@ -8,16 +8,18 @@ import java.util.Date;
 
 public class Server {
 	public static void main(String[] arg) throws IOException {
-		ServerSocket listener = new ServerSocket(9091);
+		ServerSocket listener = new ServerSocket(9090);
 		try {
 			while(true) {
+				System.out.println("begin listen");
 				Socket socket = listener.accept();
+				System.out.println("listening");
 				try {
 					PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 					out.println(new Date().toString());
 				} finally {
 					socket.close();
-					System.out.println();
+					System.out.println("closing");
 				}
 			}
 		}
